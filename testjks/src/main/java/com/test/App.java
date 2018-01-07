2ccb04cadd8f4647580a9810481a1f74c4e0def4
codewang1991 <1076953055@qq.com>
2018-01-07 20:08:26 +0800

    增加springboot依赖

--- testjks/src/main/java/com/test/App.java
+++ testjks/src/main/java/com/test/App.java
@@ -1,13 +1,27 @@
 package com.test;
 
+import javax.xml.ws.RespectBinding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
+@RestController
 public class App 
 {
+	@GetMapping(value="/testjks")
+	public String test(){
+		return "测试测试。。。。。";
+	}
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
